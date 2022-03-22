0ab44dcdc46341a181be2bff896e4181cd2f1fed
Xenia <m.tunahanbas90@gmail.com>
2022-03-22 18:48:29 +0300

    New netbeans cpp project : linkage is created

---
+++ Linkage/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xenia
+ *
+ * Created on March 22, 2022, 6:46 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
