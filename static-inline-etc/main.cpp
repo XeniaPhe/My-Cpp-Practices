26f898ee0e396ade8f205135157a896682823950
Xenia <m.tunahanbas90@gmail.com>
2022-03-23 00:08:01 +0300

    new project is created

---
+++ static-inline-etc/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xenia
+ *
+ * Created on March 23, 2022, 12:04 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
