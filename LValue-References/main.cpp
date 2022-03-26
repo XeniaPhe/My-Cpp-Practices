db3bd8d88177eabe96db064861786056c71c62e8
Xenia <m.tunahanbas90@gmail.com>
2022-03-26 17:52:16 +0300

    lvalue references are covered

---
+++ LValue-References/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: xenia
+ *
+ * Created on March 26, 2022, 4:05 PM
+ */
+
+#include <iostream>
+#include <string>
+
+void printLine(const std::string& str)	//Lvalue references are used to pass objects (mostly classes)
+{
+	std::cout<<str<<std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+	int x = 5;
+	double y = 3.14;
+	int& ref {x};		//Created an lvalue reference to the lvalue x,we say ref is bound to variable x
+	double& ref1 = y;
+	
+	
+	//We can use these references to access to their bound object's value or modify it,however the reference itself is not an object
+	
+	//int& invalidRef1; => references must be initialized
+	
+	//int& invalidRef2{5}; => references can't be bound to rvalues since rvalues are immediate values
+	
+	const char A = 'A';
+	//char& invalidRef3 = A; => lvalue reference can't reference a const object
+	
+	const char& constRef = A;	//However a const lvalue reference can reference a const object and can be used to access to its value but can't modify it
+	
+	bool isGrounded=false;
+	const bool& constRef1 = isGrounded;		//const lvalue references can also reference modifiable (non-const)objects 
+	
+	const float& constRef2 = 6;		//const lvalue references can also be given an rvalue and it will create a temporary object
+	
+	return 0;
+}
+
