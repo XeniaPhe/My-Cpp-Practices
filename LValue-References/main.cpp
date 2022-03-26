76d9dcace77151d3c44b41ad6d09462e671d2682
Xenia <m.tunahanbas90@gmail.com>
2022-03-26 18:22:24 +0300

    some changes

--- LValue-References/main.cpp
+++ LValue-References/main.cpp
@@ -19,9 +19,19 @@
 #include <iostream>
 #include <string>
 
-void printLine(const std::string& str)	//Lvalue references are used to pass objects (mostly classes)
+void printByValue(std::string val) // The function parameter is a copy of str
 {
-	std::cout<<str<<std::endl;
+	std::cout << val << '\n'; // print the value via the copy
+}
+
+void printByReference(const std::string& ref) // The function parameter is a reference that binds to str
+{
+	std::cout << ref << '\n'; // print the value via the reference
+}
+
+void printByAddress(const std::string* ptr) // The function parameter is a pointer that holds the address of str
+{
+	std::cout << *ptr << '\n'; // print the value via the dereferenced pointer
 }
 
 
